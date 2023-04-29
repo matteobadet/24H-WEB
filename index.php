c5f462f878f9216b31c986735f1e73e62ab69e6e
matteobadet <matteo.badet@iut-dijon.u-bourgogne.fr>
2023-04-29 23:58:37 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Bar Chez Nous</title>
+    <meta name="description" content="Le meilleur bar de la ville !">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <header>
+      <h1>Bar Chez Nous</h1>
+      <nav>
+        <ul>
+          <li><a href="#apropos">À Propos</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section id="apropos">
+        <h2>À Propos</h2>
+        <p>Bienvenue au Bar Chez Nous, l'endroit idéal pour prendre un verre entre amis ou collègues. Notre bar propose une large sélection de boissons alcoolisées et non alcoolisées, ainsi que des snacks pour accompagner vos boissons.</p>
+      </section>
+      <section id="menu">
+        <h2>Menu</h2>
+        <ul>
+          <li>Bière pression</li>
+          <li>Vin rouge, blanc et rosé</li>
+          <li>Cocktails classiques et originaux</li>
+          <li>Soft drinks</li>
+          <li>Planches apéritives</li>
+          <li>Assiettes de charcuterie et fromages</li>
+        </ul>
+      </section>
+      <section id="contact">
+        <h2>Contact</h2>
+        <p>Vous avez des questions ou des commentaires sur notre bar ? Contactez-nous par téléphone ou par e-mail :</p>
+        <ul>
+          <li>Téléphone : 01 23 45 67 89</li>
+          <li>E-mail : contact@barcheznous.com</li>
+        </ul>
+      </section>
+    </main>
+    <footer>
+      <p>Bar Chez Nous - Tous droits réservés</p>
+    </footer>
+  </body>
+</html>
